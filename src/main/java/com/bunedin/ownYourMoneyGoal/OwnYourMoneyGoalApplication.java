11410a444b2c7d9c30a04c24a5e8ffdf059acf22
Bunedin <bunedin@Bunedinui-MacBookAir.local>
2022-03-21 21:56:34 +0900

    init

---
+++ src/main/java/com/bunedin/ownYourMoneyGoal/OwnYourMoneyGoalApplication.java
@@ -0,0 +1,13 @@
+package com.bunedin.ownYourMoneyGoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnYourMoneyGoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnYourMoneyGoalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bunedin/ownYourMoneyGoal/OwnYourMoneyGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunedin.ownYourMoneyGoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnYourMoneyGoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
